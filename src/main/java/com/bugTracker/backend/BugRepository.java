0a86da17cbbbc41953a9b9ebcdf472968a77c258
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-06 11:55:18 +0500

    BugRepository: Created new custom query existsById to check if a bug is already found in the records or not BugRepository: Added @Query annotation to existsById

--- src/main/java/com/bugTracker/backend/BugRepository.java
+++ src/main/java/com/bugTracker/backend/BugRepository.java
@@ -1,8 +1,11 @@
 package com.bugTracker.backend;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BugRepository extends JpaRepository<Bug, Integer> {
+    @Query
+    boolean existsById(Integer id);
 }
