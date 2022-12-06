2da3ee5394e2b46fc03ea58f6a850a6442da1f41
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-06 11:09:10 +0500

    Duplicate: Added new field id Duplicate: Added @Id, @GeneratedValue(strategy = GenerationType.IDENTITY) annotation

--- src/main/java/com/bugTracker/backend/Duplicate.java
+++ src/main/java/com/bugTracker/backend/Duplicate.java
@@ -1,7 +1,6 @@
 package com.bugTracker.backend;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,4 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class Duplicate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
