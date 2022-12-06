15b957b77b02f2d8b271c0fb7fa97ffa0daa54a9
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-06 13:20:22 +0500

    QuestionSupportService: Injected QuestionSupportService layer using @InjectMocks

--- src/test/java/com/bugTracker/backend/QuestionSupportServiceTest.java
+++ src/test/java/com/bugTracker/backend/QuestionSupportServiceTest.java
@@ -1,10 +1,13 @@
 package com.bugTracker.backend;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuestionSupportServiceTest {
+    @InjectMocks
+    private QuestionSupportService questionSupportService;
 
     @Test
     void shouldCheckBugValidation() {
