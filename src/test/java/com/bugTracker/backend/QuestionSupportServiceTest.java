db6327ed2674d8d767cf4c4cc129a64c0d52d862
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-06 13:18:30 +0500

    Created void checkBugValidation test

---
+++ src/test/java/com/bugTracker/backend/QuestionSupportServiceTest.java
@@ -0,0 +1,12 @@
+package com.bugTracker.backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionSupportServiceTest {
+
+    @Test
+    void checkBugValidation() {
+    }
+}
