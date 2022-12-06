020f434f36cec9ab0179c48ab5569bd50bea4b5d
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-06 11:44:34 +0500

    DuplicateService: Injected DuplicateRepository via contructor-based injection

--- src/main/java/com/bugTracker/backend/DuplicateService.java
+++ src/main/java/com/bugTracker/backend/DuplicateService.java
@@ -1,9 +1,14 @@
 package com.bugTracker.backend;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
+@Slf4j
 public class DuplicateService {
+    private DuplicateRepository duplicateRepository;
     public Boolean isBugDuplicate(Bug bug) {
         return false;
     }
