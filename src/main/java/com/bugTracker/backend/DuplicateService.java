cedd468aeb9fc0676f6a29920d8836180d99c5bc
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-06 10:55:00 +0500

    DuplicateService: Added @Service annotation DuplicateService: Extracted and isolated isBugDuplicate method from BugService to DuplicateService

--- src/main/java/com/bugTracker/backend/DuplicateService.java
+++ src/main/java/com/bugTracker/backend/DuplicateService.java
@@ -1,2 +1,10 @@
-package com.bugTracker.backend;public class DuplicateService {
+package com.bugTracker.backend;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DuplicateService {
+    public Boolean isBugDuplicate(Bug bug) {
+        return false;
+    }
 }
