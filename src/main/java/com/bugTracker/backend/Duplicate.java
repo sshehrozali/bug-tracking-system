5d1833e8d6e7195a3ca3ad8bb24b95585c7510a3
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-06 11:07:55 +0500

    Duplicate: Added @Entity, @Table, @AllArgsConstructor, @NoArgsConstructor, @Data and @Builder annotations

--- src/main/java/com/bugTracker/backend/Duplicate.java
+++ src/main/java/com/bugTracker/backend/Duplicate.java
@@ -1,4 +1,17 @@
 package com.bugTracker.backend;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Duplicate {
 }
