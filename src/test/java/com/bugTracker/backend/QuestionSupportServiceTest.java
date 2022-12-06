dba0cf499340391b4aa917b6ec1c4003c274782d
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-06 13:38:34 +0500

    QuestionSupportServiceTest: Implemented shouldCheckBugValidation method QuestionSupportServiceTest: Annotated class with @ExtendWith(Mockito.class)

--- src/test/java/com/bugTracker/backend/QuestionSupportServiceTest.java
+++ src/test/java/com/bugTracker/backend/QuestionSupportServiceTest.java
@@ -1,15 +1,26 @@
 package com.bugTracker.backend;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class QuestionSupportServiceTest {
     @InjectMocks
     private QuestionSupportService questionSupportService;
 
     @Test
+    @DisplayName("Should Check Bug Validation")
     void shouldCheckBugValidation() {
+        Bug testBug = Bug.builder()
+                .bugMessage("Hello I am a test bug")
+                .build();
+        boolean actual = questionSupportService.checkBugValidation(testBug);
+        assertEquals(false, actual);
     }
 }
