4f83012da5d27ff826d7e650bdf250f559a53f4c
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-06 14:43:33 +0500

    QuestionSupportService: Implemented condition to check if a bug message is empty or not

--- src/main/java/com/bugTracker/backend/QuestionSupportService.java
+++ src/main/java/com/bugTracker/backend/QuestionSupportService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class QuestionSupportService {
     public Boolean checkBugValidation(Bug bug) {
+        if (bug.getBugMessage().isBlank()) {
+            return false;
+        }
         return true;
     }
 }
