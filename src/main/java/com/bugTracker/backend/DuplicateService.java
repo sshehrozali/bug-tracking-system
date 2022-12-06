fb7e7d96c7987cb3b9833f67306693b8a300e7d6
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-06 12:28:57 +0500

    DuplicateService: Created new Duplicate object via builder pattern DuplicateService: Saved new duplicate in Duplicate DuplicateService: returned true if bugExists DuplicateService: else returned false

--- src/main/java/com/bugTracker/backend/DuplicateService.java
+++ src/main/java/com/bugTracker/backend/DuplicateService.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @AllArgsConstructor
 @Slf4j
@@ -13,6 +15,12 @@ public class DuplicateService {
     public Boolean isBugDuplicate(Bug bug) {
         boolean bugExists = bugRepository.existsById(bug.getId());
         if (bugExists) {
+            Duplicate duplicate = Duplicate.builder()
+                    .markedAt(LocalDateTime.now())
+                    .bug(bug)
+                    .build();
+            Duplicate duplicateFlushed = duplicateRepository.saveAndFlush(duplicate);
+            log.info("New duplicate marked: " + duplicateFlushed.getId());
             return true;    // Bug already found
         }
 
