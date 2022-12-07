00b8380627e0031bf44c5a8ed267410ac54e4220
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-07 11:35:37 +0500

    BugServiceTest: Refactored name from newBugReport to shouldReturnBugMsgCantBeEmpty BugServiceTest: Injected service layer via @InjectMocks BugServiceTest: Created new object of NewBugRequest

--- src/test/java/com/bugTracker/backend/BugServiceTest.java
+++ src/test/java/com/bugTracker/backend/BugServiceTest.java
@@ -1,12 +1,20 @@
 package com.bugTracker.backend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class BugServiceTest {
+    @InjectMocks
+    private BugService bugService;
 
     @Test
-    void newBugReport() {
+    void shouldReturnBugMsgCantBeEmpty() {
+        NewBugRequest newBugRequest = new NewBugRequest("", "");
+
     }
 }
