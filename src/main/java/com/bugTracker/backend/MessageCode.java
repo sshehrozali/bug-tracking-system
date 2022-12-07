117a3c787493f9fa65f9cef33cb67d5b77990dfc
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-07 12:53:32 +0500

    MessageCode: Implemented new enum field

--- src/main/java/com/bugTracker/backend/MessageCode.java
+++ src/main/java/com/bugTracker/backend/MessageCode.java
@@ -3,5 +3,6 @@ package com.bugTracker.backend;
 public enum MessageCode {
     INVALID_BUG_MESSAGE,
     DUPLICATE_BUG_MESSAGE,
-    EMPTY_BUG_MESSAGE
+    EMPTY_BUG_MESSAGE,
+    DETAILS_NOT_PROVIDED
 }
