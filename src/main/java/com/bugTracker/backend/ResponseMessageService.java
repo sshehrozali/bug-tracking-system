ad700de5378ad06ad0d597a09a8a3237f66d93df
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-07 12:48:26 +0500

    ResponseMessageService: Implemented HashMap<Enum, String> for storing all custom messages ResponseMessageService: Returned message matched with key

--- src/main/java/com/bugTracker/backend/ResponseMessageService.java
+++ src/main/java/com/bugTracker/backend/ResponseMessageService.java
@@ -1,10 +1,21 @@
 package com.bugTracker.backend;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
 @Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseMessageService {
+    public String getMessage(Enum code) {
+        HashMap<Enum, String> messages = new HashMap<>();
+        messages.put(MessageCode.EMPTY_BUG_MESSAGE, "Invalid Bug request. Can't be empty.");
+        messages.put(MessageCode.DUPLICATE_BUG_MESSAGE, "Duplicate Bug found.");
+        messages.put(MessageCode.INVALID_BUG_MESSAGE, "Not a valid Bug.");
 
+        return messages.get(code);
+    }
 }
