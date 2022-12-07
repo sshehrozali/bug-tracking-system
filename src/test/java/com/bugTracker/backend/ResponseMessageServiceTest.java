06d9bd550422ab11770375a3c7f56510e53bf08f
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-07 14:12:23 +0500

    ResponseMessageServiceTest: Implemented shouldReturnEmptyBugMessage()

--- src/test/java/com/bugTracker/backend/ResponseMessageServiceTest.java
+++ src/test/java/com/bugTracker/backend/ResponseMessageServiceTest.java
@@ -1,10 +1,12 @@
 package com.bugTracker.backend;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -13,6 +15,8 @@ class ResponseMessageServiceTest {
     private ResponseMessageService underTest;
 
     @Test
+    @DisplayName("Should return error message when message code is not matched")
     void shouldReturnEmptyBugMessage() {
+        assertThat(underTest.getMessage(MessageCode.ERROR)).isNull();
     }
 }
