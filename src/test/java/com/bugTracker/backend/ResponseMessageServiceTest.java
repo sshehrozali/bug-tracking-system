02e963b11a9caec69788a2f4611090e4b88e5d97
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-07 14:06:33 +0500

    ResponseMessageServiceTest: Refactored name getMessage() to shouldReturnEmptyBugMessage() ResponseMessageServiceTest: Injected ResponseMessageService layer via @InjectMocks ResponseMessageServiceTest: Added @ExtendWith(MockitoExtension.class)

--- src/test/java/com/bugTracker/backend/ResponseMessageServiceTest.java
+++ src/test/java/com/bugTracker/backend/ResponseMessageServiceTest.java
@@ -4,20 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-class BugServiceTest {
+class ResponseMessageServiceTest {
     @InjectMocks
-    private BugService underTest;
+    private ResponseMessageService underTest;
 
     @Test
-    void shouldReturnBugMsgCantBeEmpty() {
-        NewBugRequest newBugRequest = new NewBugRequest("", "");
-        ResponseEntity<?> actual = underTest.newBugReport(newBugRequest);
-
-
+    void shouldReturnEmptyBugMessage() {
     }
 }
