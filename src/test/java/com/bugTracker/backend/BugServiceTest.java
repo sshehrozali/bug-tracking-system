b13217c6c290140f6deaa1cce2072df3e1dc3642
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-07 15:31:34 +0500

    BugServiceTest: Injected ResponseMessageService using @InjectMocks

--- src/test/java/com/bugTracker/backend/BugServiceTest.java
+++ src/test/java/com/bugTracker/backend/BugServiceTest.java
@@ -6,18 +6,23 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class BugServiceTest {
     @InjectMocks
     private BugService underTest;
+    @InjectMocks
+    private ResponseMessageService responseMessageService;
 
     @Test
     void shouldReturnBugMsgCantBeEmpty() {
         NewBugRequest newBugRequest = new NewBugRequest("", "");
         ResponseEntity<?> actual = underTest.newBugReport(newBugRequest);
 
-
+        CustomAPIResponse customAPIResponse = new CustomAPIResponse()
+        ResponseEntity<?> expected =
+        assertThat(actual).isEqualTo()
     }
 }
