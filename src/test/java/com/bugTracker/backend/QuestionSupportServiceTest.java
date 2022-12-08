b62038f66a3df503139cf785789c386ee092ad50
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-08 15:45:09 +0500

    QuestionSupportService: Refactored logic

--- src/test/java/com/bugTracker/backend/QuestionSupportServiceTest.java
+++ src/test/java/com/bugTracker/backend/QuestionSupportServiceTest.java
@@ -3,11 +3,7 @@ package com.bugTracker.backend;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuestionSupportServiceTest {
