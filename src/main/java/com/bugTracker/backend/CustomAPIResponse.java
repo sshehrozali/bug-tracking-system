c0923e108c8b228e5bc87ec628515b786dc0a451
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-09 11:09:41 +0500

    CustomAPIResponse: Added new HttpStatus field

--- src/main/java/com/bugTracker/backend/CustomAPIResponse.java
+++ src/main/java/com/bugTracker/backend/CustomAPIResponse.java
@@ -2,9 +2,11 @@ package com.bugTracker.backend;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Builder
 public record CustomAPIResponse(
-        String msg
+        String msg,
+        HttpStatus httpStatus
 ) {
 }
