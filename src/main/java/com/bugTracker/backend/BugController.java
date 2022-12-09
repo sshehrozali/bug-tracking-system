32a5161de70e85bce605dad66596de5c29c370ae
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-09 11:02:35 +0500

    BugController: Injected ResponseMessageService dependency

--- src/main/java/com/bugTracker/backend/BugController.java
+++ src/main/java/com/bugTracker/backend/BugController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class BugController {
     private final BugService bugService;
-
+    private final ResponseMessageService responseMessageService;
 
     @PostMapping("/new/bug")
     public ResponseEntity<?> newBug(@RequestBody NewBugRequest newBugRequest) {
